1241346e853f187e072d302ddc7517a59fc552b2
yuu <yuukuramu@gmail.com>
2022-09-11 14:37:00 +0300

    connect mysql with cash-system

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-#!/usr/bin/python3
 
 from pkg.__init__ import Cash_register
 
-def main() -> None:
+from config import DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWD
+from database import _Database_
 
+def main() -> None: 
+
+    db = _Database_(DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWD)
     CR = Cash_register("DX6431LL", 30000)
 
-    print(CR.copyrights())
+    print( CR.copyrights())
+
+
+
+
 
 if __name__ == "__main__":
     main()
