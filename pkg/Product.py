70ba5fc50a84575916ac2f588ca22fac1ebb4226
yuu <yuukuramu@gmail.com>
2022-12-05 11:47:44 +0200

    finish base functions.

--- pkg/Product.py
+++ pkg/Product.py
@@ -1,8 +1,73 @@
+from database import connection
+
 class Product:
+            
+    def generate_barcode(self, sellerid: int, categoryid: int) -> str:
+        """generate barcode
 
-    def generate_barcode(self) -> None:
-        # 756000233 generate barcode system - 1*3 = seller id 2*3 = product id 3*3 = category id
-        pass
+        Args:
+            sellerid (int): seller company id
+            categoryid (int): category id
 
-    def get_barcode(self) -> str:
-        pass
+        Returns:
+            str: barcode
+        """
+
+
+        with connection.cursor() as cursor:
+            sql = f"SELECT id FROM products ORDER BY id DESC LIMIT 1;"
+            cursor.execute(sql)
+            lastest_row = cursor.fetchone()
+
+            for productid in lastest_row.values():
+                return f"{sellerid}{categoryid}{productid + 1}"
+
+
+    def get_barcode(self, product_name: str) -> list:
+        """
+
+        Args:
+            product_name (str): name of product
+
+        Returns:
+            list: example - (barcode:445)
+        """
+        with connection.cursor() as cursor:
+            sql = f"SELECT barcode FROM products WHERE name = '{product_name}'"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            
+            return result
+
+    def add_product(self, barcode: int, name: str, count: int, weight: float, category: int, price: float) -> None:
+        """Add product in database
+
+        Args:
+            barcode (int): generate barcode
+            name (str): name of product
+            count (int): count of product
+            weight (float): products weight
+            category (int): category of product
+            price (float): product price
+        """
+
+        with connection.cursor() as cursor:
+            sql = f"INSERT INTO `products` (`barcode`, `name`, `count`, `weight`, `category`, `price`) VALUES ('{barcode}', '{name}', '{count}', '{weight}', '{category}', '{price}')"
+            cursor.execute(sql)
+            connection.commit()
+
+    def get_product(self, barcode: int) -> list:
+        """Get all information about product with barcode
+
+        Args:
+            barcode (int): 
+
+        Returns:
+            list: all information about product
+        """
+        with connection.cursor() as cursor:
+            sql = f"SELECT * FROM products WHERE barcode = '{barcode}'"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+        
+        return result
