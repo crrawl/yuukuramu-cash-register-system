3e3fa9253923da5481fe6b9ef463cbe63d233e92
yuu <yuukuramu@gmail.com>
2023-02-08 01:13:54 +0200

    Add UI commands

---
+++ addseller.py
@@ -0,0 +1,24 @@
+from pkg.Storage import Storage
+import click
+from os import system
+@click.command()
+@click.argument('seller_name')
+@click.argument('category_id')
+def addseller(seller_name, category_id):
+    """Purchashe products and get receipt
+
+    example, python3 addseller.py [seller_name] [category_id]
+    example, python3 addseller.py "Evil Corp" 4
+
+    4 is category_id from 'PC'
+
+    You can check categories with,
+
+    python3 showcategories.py
+    """
+
+    Storage.add_seller(1, seller_name, category_id)
+
+if __name__ == "__main__":
+    system("cls")
+    addseller()
